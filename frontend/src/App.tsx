941bee83a279ee82bb6983db4d6485f16807de83
boon2187 <fumiya.mai.ito@gmail.com>
2025-12-15 11:07:16 +0900

    入力用のTodoFormコンポーネントを作成し、App.tsxに反映

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Header from "./components/layout/Header";
 import TodoList from "./components/todo/TodoList";
+import TodoForm from "./components/todo/TodoForm";
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center">
       <Header />
+      <TodoForm />
       <TodoList />
     </div>
   );
---
+++ frontend/src/components/todo/TodoForm.tsx
@@ -0,0 +1,3 @@
+export default function TodoForm() {
+  return <div className="bg-blue-100">TodoForm</div>;
+}
