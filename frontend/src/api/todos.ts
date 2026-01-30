ecbf4d2603af54299c70ce3824c4016fad1ef30f
boon2187 <fumiya.mai.ito@gmail.com>
2026-01-30 23:17:07 +0900

    API通信関数の実装（一覧、新規作成、更新、完了のとぐる）

--- frontend/src/api/todos.ts
+++ frontend/src/api/todos.ts
@@ -5,33 +5,37 @@ const API_BASE = "/api/todos";
 
 // 一覧取得
 export const getTodos = async (): Promise<Todo[]> => {
-  // TODO: 実装
-  throw new Error("Not implemented");
+  // todoを返す
+  const response = await axios.get<Todo[]>(API_BASE);
+  return response.data;
 };
 
 // 新規作成
 export const createTodo = async (data: TodoCreate): Promise<Todo> => {
-  // TODO: 実装
-  throw new Error("Not implemented");
+  // dataつかって、新しいtodoを登録
+  const response = await axios.post<Todo>(API_BASE, data);
+  return response.data;
 };
 
 // 更新
 export const updateTodo = async (
   id: number,
-  data: TodoUpdate
+  data: TodoUpdate,
 ): Promise<Todo> => {
-  // TODO: 実装
-  throw new Error("Not implemented");
+  // idのtodoをdataで更新
+  const response = await axios.patch<Todo>(`${API_BASE}/${id}`, data);
+  return response.data;
 };
 
 // 削除
 export const deleteTodo = async (id: number): Promise<void> => {
-  // TODO: 実装
-  throw new Error("Not implemented");
+  // idのtodoを削除
+  await axios.delete(`${API_BASE}/${id}`);
 };
 
 // 完了トグル
 export const toggleTodo = async (id: number): Promise<Todo> => {
-  // TODO: 実装
-  throw new Error("Not implemented");
+  // idのtodoのis_completedをトグル
+  const response = await axios.patch<Todo>(`${API_BASE}/${id}/toggle`);
+  return response.data;
 };
