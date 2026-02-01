ab512d05d3352150f293431e38e3112156584f90
boon2187 <fumiya.mai.ito@gmail.com>
2026-02-01 11:57:13 +0900

    カスタムフックスの戻り地の方を定義して、カスタムフックスのステートの定義まで

--- frontend/src/hooks/useTodos.ts
+++ frontend/src/hooks/useTodos.ts
@@ -1,3 +1,25 @@
-export const useTodos = () => {
-  // TODO: 実装
+import { Todo, TodoCreate, TodoUpdate } from "@/types/todo";
+import { useState } from "react";
+
+interface UseTodosReturn {
+  // state
+  todos: Todo[];
+  loading: boolean;
+  error: string | null;
+
+  // actions
+  fetchTodos: () => Promise<void>;
+  addTodo(data: TodoCreate): Promise<void>;
+  editTodo(id: number, data: TodoUpdate): Promise<void>;
+  removeTodo(id: number): Promise<void>;
+  toggleTodoCompletion(id: number): Promise<void>;
+}
+
+export const useTodos = (): UseTodosReturn => {
+  // stateの定義
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  // actionsの定義
 };
