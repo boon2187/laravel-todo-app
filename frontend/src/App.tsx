eaf8ae734e1d071664ab95c6be15a8922583d8d9
boon2187 <fumiya.mai.ito@gmail.com>
2026-02-01 17:28:58 +0900

    親コンポーネントでカスタムフックを呼んで、追加処理をFormコンポーネントに渡す

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,23 @@ import { useState } from "react";
 import Header from "./components/layout/Header";
 import TodoList from "./components/todo/TodoList";
 import TodoForm from "./components/todo/TodoForm";
+import { useTodos } from "./hooks/useTodos";
 
 function App() {
+  const {
+    todos,
+    loading,
+    error,
+    fetchTodos,
+    addTodo,
+    editTodo,
+    removeTodo,
+    toggleTodoCompletion,
+  } = useTodos();
   return (
     <div className="flex flex-col items-center justify-center">
       <Header />
-      <TodoForm />
+      <TodoForm onAddTodo={addTodo} />
       <TodoList />
     </div>
   );
--- frontend/src/components/todo/TodoForm.tsx
+++ frontend/src/components/todo/TodoForm.tsx
@@ -1,3 +1,20 @@
-export default function TodoForm() {
+import { TodoCreate } from "@/types/todo";
+import { useState } from "react";
+
+interface TodoFormProps {
+  onAddTodo: (data: TodoCreate) => Promise<void>;
+}
+
+export default function TodoForm({ onAddTodo }: TodoFormProps) {
+  const [title, setTitle] = useState("");
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!title.trim()) {
+      return;
+    }
+    const newTodo: TodoCreate = { title: title.trim() };
+    await onAddTodo(newTodo);
+    setTitle("");
+  };
   return <div className="bg-blue-100">TodoForm</div>;
 }
